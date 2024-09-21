95033e45e956f0691eccc1062fe16096c08751f1
hopo55 <hopo5798@gmail.com>
2024-09-21 17:16:10 +0900

    fixed epochs

--- scripts/resnet18/run_sparse_ncm_cifar10.sh
+++ scripts/resnet18/run_sparse_ncm_cifar10.sh
@@ -3,10 +3,10 @@ METHOD="derpp"     # "er" or "derpp"
 DATASET="seq-cifar10"
 DEVICE="PC"
 BUFFER_SIZE=500
-SPARSE=0.25
+SPARSE=0.75
 GLOBAL_BATCH_SIZE="32"
 GPU_ID=0
-PATH_TO_SPARCL=/home/cal-06/heonsung/SparCL-NCM # change to your own path
+PATH_TO_SPARCL=/home/ml/HeonSung/SparCL-NCM # change to your own path
 
 # magnitude-based 1 shot retraining
 ARCH="resnet" # 
@@ -14,7 +14,7 @@ DEPTH="18"
 PRUNE_ARGS="--sp-retrain --sp-prune-before-retrain"
 LOAD_CKPT="XXXXX.pth.tar"     # automatically train from scratch if the given checkpoint model is not found
 INIT_LR="0.03"
-EPOCHS="250"
+EPOCHS="50"
 WARMUP="8"
 
 SPARSITY_TYPE="irregular"
--- scripts/resnet18/run_sparse_ncm_cifar100.sh
+++ scripts/resnet18/run_sparse_ncm_cifar100.sh
@@ -6,7 +6,7 @@ BUFFER_SIZE=500
 SPARSE=0.25
 GLOBAL_BATCH_SIZE="32"
 GPU_ID=0
-PATH_TO_SPARCL=/home/cal-06/heonsung/SparCL-NCM # change to your own path
+PATH_TO_SPARCL=/home/ml/HeonSung/SparCL-NCM # change to your own path
 
 # magnitude-based 1 shot retraining
 ARCH="resnet" # 
@@ -14,7 +14,7 @@ DEPTH="18"
 PRUNE_ARGS="--sp-retrain --sp-prune-before-retrain"
 LOAD_CKPT="XXXXX.pth.tar"     # automatically train from scratch if the given checkpoint model is not found
 INIT_LR="0.03"
-EPOCHS="500"
+EPOCHS="100"
 WARMUP="8"
 
 SPARSITY_TYPE="irregular"
