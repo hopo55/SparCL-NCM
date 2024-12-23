dc8cb04c482e0b1c332144b45d768393de99b261
hopo55 <hopo5798@gmail.com>
2024-12-23 15:48:19 +0900

    update scripts

--- scripts/resnet18/run_sparse_ncm_cifar10.sh
+++ scripts/resnet18/run_sparse_ncm_cifar10.sh
@@ -24,10 +24,11 @@ SP_MASK_UPDATE_FREQ="5"
 REMOVE_N=3000
 RM_EPOCH=20
 GRADIENT=0.8
+ITER=10
 # REMOVE_N=0
 # RM_EPOCH=-1
 # GRADIENT=1
-ITER=1
+# ITER=1
 
 SAVE_FOLDER="checkpoints/resnet18/paper/gradient_effi/mutate_irr/${DATASET}/buffer_${BUFFER_SIZE}/"
 cd $PATH_TO_SPARCL
